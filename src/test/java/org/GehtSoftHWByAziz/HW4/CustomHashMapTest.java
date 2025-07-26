a6a10dbdf228f386798d45ef34edd15e496e293f
SinbadTheSailor2005 <aziz.yubuza@mail.ru>
2025-07-26 12:05:52 +0300

    add parametrized testts

--- src/test/java/org/GehtSoftHWByAziz/HW4/CustomHashMapTest.java
+++ src/test/java/org/GehtSoftHWByAziz/HW4/CustomHashMapTest.java
@@ -1,32 +1,32 @@
 package org.GehtSoftHWByAziz.HW4;
 import org.GehtSoftHWByAziz.HW2.FibonacciAlgorithm.FibonacciAlgorithms;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashMap;
-
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.params.ParameterizedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CustomHashMapTest {
-  CustomHashMap<Integer, Integer> m;
-  @BeforeEach
-  void setUp() {
-    m = new CustomHashMap<>();
-  }
-  @AfterEach
-  void tearDown() {
-    m.clear();
-  }
-  @Test
-  void testAdd() {
+ static List<Map<Integer, Integer>> mapImplementations() {
+   return List.of(new CustomHashMap<Integer,Integer>(),
+           new HashMap<Integer,Integer>() );
+ }
+ @ParameterizedTest
+@MethodSource("mapImplementations")
+  void testAdd (Map<Integer,Integer>m ) {
     m.put(1, 2);
     m.put (2,3);
     assertEquals(2, m.get(1));
     assertEquals(3,m.get(2));
   }
 
-  @Test
-  void testRemove() {
+  @ParameterizedTest
+@MethodSource ("mapImplementations")
+  void testRemove (Map<Integer,Integer>m ) {
     m.put(1,1);
     m.put(2,2);
     m.remove(1);
@@ -35,8 +35,9 @@ public class CustomHashMapTest {
     assertEquals(0, m.size());
   }
 
-  @Test
-  void testPutAll() {
+  @ParameterizedTest
+@MethodSource ("mapImplementations")
+  void testPutAll (Map<Integer,Integer>m ) {
     var map = new HashMap<Integer,Integer>();
     map.put(1,1);
     map.put(2,2);
@@ -45,15 +46,17 @@ public class CustomHashMapTest {
     assertEquals(2,m.get(2));
   }
 
-  @Test
-  void testContains() {
+  @ParameterizedTest
+@MethodSource ("mapImplementations")
+  void testContains (Map<Integer,Integer>m ) {
     m.put(1,1);
     assertTrue(m.containsKey(1));
     assertTrue(m.containsValue(1));
   }
 
-  @Test
-  void testGrow() {
+  @ParameterizedTest
+@MethodSource ("mapImplementations")
+  void testGrow (Map<Integer,Integer>m ) {
     for (int i = 0; i < 100; i++) {
       m.put(i,i);
     }
