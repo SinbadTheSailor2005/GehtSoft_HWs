d4d989688c1d4268fe474f5e145b5375e47df13c
SinbadTheSailor2005 <aziz.yubuza@mail.ru>
2025-08-19 12:54:43 +0500

    add notes to annotations

--- src/main/java/org/GehtSoftHWByAziz/HW7/RESTAnnotations/CustomRestController.java
+++ src/main/java/org/GehtSoftHWByAziz/HW7/RESTAnnotations/CustomRestController.java
@@ -7,6 +7,8 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-
+/**
+ * RestController = Controller + ResponseBody
+ */
 public @interface CustomRestController {
 }
